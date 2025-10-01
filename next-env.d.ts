727d460301d28e8bfd372505c538741dac695f42
Sidnei Teixeira <sidnei.teixeira@skalelabs.com>
2025-10-01 19:37:17 +0100

    init

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,10 +1,5 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
-/// <reference types="next/navigation-types/compat/navigation" />
-
-declare module '*.md' {
-  const content: string;
-  export default content;
-}
 
 // NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
